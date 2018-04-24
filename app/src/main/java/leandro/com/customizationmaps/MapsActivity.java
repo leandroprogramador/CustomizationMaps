eb95bb2c4676ba0aac872f094774de85b76bbf7e
leandro.araujo <lean.ib@hotmail.com>
2018-04-24 14:52:48 -0300

    Customized  by JSON
    Google maps customization by json file

--- app/src/main/java/leandro/com/customizationmaps/MapsActivity.java
+++ app/src/main/java/leandro/com/customizationmaps/MapsActivity.java
@@ -1,5 +1,6 @@
 package leandro.com.customizationmaps;
 
+import android.content.res.Resources;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
@@ -8,6 +9,7 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MapStyleOptions;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
@@ -37,6 +39,13 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
+
+        try{
+            mMap.setMapStyle( MapStyleOptions.loadRawResourceStyle(this,R.raw.style));
+
+        } catch (Resources.NotFoundException ex){
+
+        }
 
         // Add a marker in Sydney and move the camera
         LatLng sydney = new LatLng(-34, 151);
